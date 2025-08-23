c06c66cd8070c2dba2b056d1d5d8512cf036c91b
mastermind-council <kwameanku@gmail.com>
2025-08-22 19:51:32 -0700

    Update next.config.js

--- mastermind-council/next.config.js
+++ mastermind-council/next.config.js
@@ -2,7 +2,20 @@
 const nextConfig = {
   experimental: {
     appDir: true,
-  }
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "script-src 'self' 'unsafe-eval' 'unsafe-inline'; object-src 'none';",
+          },
+        ],
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
