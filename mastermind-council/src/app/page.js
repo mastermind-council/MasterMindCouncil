bb0296bff14230891ac7bf66eee65253e476e13b
mastermind-council <kwameanku@gmail.com>
2025-08-23 00:26:25 -0700

    Update page.js

--- mastermind-council/src/app/page.js
+++ mastermind-council/src/app/page.js
@@ -1,6 +1,4 @@
-// src/app/page.js
-import MasterMindCouncil from "@/components/MasterMindCouncil"; // if you used the alias above
-// import MasterMindCouncil from "../components/MasterMindCouncil"; // use this line instead if you skip the alias
+import MasterMindCouncil from "../components/MasterMindCouncil";
 
 export default function Page() {
   return <MasterMindCouncil />;
