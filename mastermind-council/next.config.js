c3b05c27d72f4fd866a80d37ea2057554899a150
mastermind-council <kwameanku@gmail.com>
2025-08-24 09:22:08 -0700

    Update next.config.js

--- mastermind-council/next.config.js
+++ mastermind-council/next.config.js
@@ -1,18 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   async headers() {
     return [
       {
-        source: '/(.*)',
+        source: '/:path*',
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "script-src 'self' 'unsafe-eval' 'unsafe-inline'; object-src 'none';",
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:;",
           },
         ],
       },
     ];
   },
-}
+};
 
 module.exports = nextConfig;
